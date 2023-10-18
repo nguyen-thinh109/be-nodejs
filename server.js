ab13c26d057815c63b4523e1f288ef61d034a06c
Nguyễn Thịnh <110882542+nguyen-thinh109@users.noreply.github.com>
2023-10-18 18:07:29 +0700

    pages

--- server.js
+++ server.js
@@ -4,12 +4,18 @@ const app = express();
 const port = process.env.port;
 
 app.get('/', (req, res) => {
-  res.send('<b>Hello World!</b>'+ process.env.HOST_NAME + process.env.port)
+  // res.send('<b>Hello World!</b>'+ process.env.HOST_NAME + process.env.port)
+
+  res.sendFile('./views/home.html', { root: __dirname})
 })
 
 app.get('/test', (req, res) => {
     res.send('You are on the new branch!---')
   })
+
+app.use((req, res) => {
+  res.status(404).sendFile('./views/404.html', { root: __dirname})
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
