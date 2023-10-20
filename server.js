7467f93d2d4f602e6fcf3cb667dabae0c152ff08
Nguyễn Thịnh <110882542+nguyen-thinh109@users.noreply.github.com>
2023-10-20 18:18:19 +0700

    aaaaaa

--- server.js
+++ server.js
@@ -2,11 +2,22 @@ const express = require('express');
 require('dotenv').config();
 const app = express();
 const port = process.env.port;
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+const dbURI = 'mongodb+srv://tnguyen01:tnguyen01@nodejsbe.crgpri3.mongodb.net/userInfo.user?retryWrites=true&w=majority';
+mongoose
+  .connect("dbURI", { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((res) => console.log('connected to db', res))
+  .catch((error) => console.log(error));
+
+app.use(morgan('dev'));
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
   // res.send('<b>Hello World!</b>'+ process.env.HOST_NAME + process.env.port)
 
-  res.sendFile('./views/home.html', { root: __dirname})
+  res.sendFile('./views/index.html', { root: __dirname})
 })
 
 app.get('/test', (req, res) => {
