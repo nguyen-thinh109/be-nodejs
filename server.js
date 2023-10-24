440d7521c946f49a94c8ac68d23bdb5ae63ee5fb
Nguyễn Thịnh <110882542+nguyen-thinh109@users.noreply.github.com>
2023-10-24 18:11:07 +0700

    123654

--- server.js
+++ server.js
@@ -4,73 +4,51 @@ const app = express();
 const port = process.env.port;
 const morgan = require('morgan');
 const mongoose = require('mongoose');
-const { MongoClient, ServerApiVersion } = require('mongodb');
-
-const client = new MongoClient(process.env.MONGO_URL, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
-
-// async function run() {
-//   try {
-//     // Connect the client to the server	(optional starting in v4.7)
-//     await client.connect();
-//     const database = client.db("userInfo");
-//     const userInfo = await database.collection('user').find().toArray();
-
-//     console.log("Pinged your deployment. You successfully connected to MongoDB!", userInfo);
-//   } finally {
-//     // Ensures that the client will close when you finish/error
-//     await client.close();
-//   }
-// }
-
-// run().catch(console.dir);
-
-// mongoose.connect(process.env.MONGO_URL)
-//   .then(async (res) => {
-//     const database = client.db("userInfo");
-//     const skills = await database.collection('skills').find().toArray();
-
-//     console.log("Pinged your deployment. You successfully connected to MongoDB!", skills);
-//   })
-//   .catch(() => {
-//     console.log("Couldn't connect to MongoDB");
-//   })
 
 app.use(morgan('dev'));
 app.use(express.static('public'));
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
-app.set("view engine","ejs");
-app.set("views","./views");
+app.set("view engine", "ejs");
+app.set("views", "./views");
 
-app.get('/', (req, res) => {
-  // res.send('<b>Hello World!</b>'+ process.env.HOST_NAME + process.env.port)
-  // res.sendFile('./views/index.html', { root: __dirname})
+var pendingTasks = [];
 
-  mongoose.connect(process.env.MONGO_URL)
-  .then(async () => {
-    const database = client.db("userInfo");
-    const skills = await database.collection('skills').find().toArray();
-    res.render('index.ejs', {skills: skills});
-
-    console.log("Pinged your deployment. You successfully connected to MongoDB!", skills);
-  })
-  .catch(() => {
-    console.log("Couldn't connect to MongoDB");
-  })
-  
-})
-
-app.get('/test', (req, res) => {
+app.get('/', async (req, res) => {
   mongoose.connect(process.env.MONGO_URL)
     .then(async () => {
-      const database = client.db("userInfo");
-      const skills = await database.collection('skills').insertOne({ skill: 'test 1' });
-      console.log("Added to MongoDB!", skills);
+      const database = client.db("todo");
+      const tasks = await database.collection('pending').find().toArray();
+      pendingTasks = [...tasks];
+      
+      res.render('index.ejs', { tasks: pendingTasks });
+    })
+    .catch((err) => {
+      console.log("Couldn't connect to MongoDB", err);
+    })
+})
+
+app.post('/completed', (req, res) => {
+  console.log('request', req.body);
+  // mongoose.connect(process.env.MONGO_URL)
+  // .then(async () => {
+  //   const database = client.db("todo");
+  //   const tasks = await database.collection('pending').find().toArray();
+  //   res.render('index.ejs', {tasks: tasks});
+  // })
+  // .catch(() => {
+  //   console.log("Couldn't connect to MongoDB");
+  // })
+
+})
+
+app.get('/completed', (req, res) => {
+  mongoose.connect(process.env.MONGO_URL)
+    .then(async () => {
+      const database = client.db("todo");
+      const completed = await database.collection('completed').find().toArray();
+      res.render('completed.ejs', { tasks: completed });
     })
     .catch(() => {
       console.log("Couldn't connect to MongoDB");
@@ -79,7 +57,7 @@ app.get('/test', (req, res) => {
 })
 
 app.use((req, res) => {
-  res.status(404).sendFile('./views/404.html', { root: __dirname})
+  res.status(404).sendFile('./views/404.html', { root: __dirname })
 })
 
 app.listen(port, () => {
