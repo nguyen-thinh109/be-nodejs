931127b8bf397469a4bd08f6fec1211548e2b7ae
Nguyễn Thịnh <110882542+nguyen-thinh109@users.noreply.github.com>
2023-10-25 18:21:13 +0700

    post, delete records

--- server.js
+++ server.js
@@ -1,65 +1,82 @@
-const express = require('express');
-require('dotenv').config();
+const express = require("express");
+require("dotenv").config();
 const app = express();
 const port = process.env.port;
-const morgan = require('morgan');
-const mongoose = require('mongoose');
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const Pending = require("./models/pending");
+const Complete = require("./models/complete");
 
-app.use(morgan('dev'));
-app.use(express.static('public'));
-app.use(express.json()) // for parsing application/json
-app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+//Middlewares & static files
+app.use(morgan("dev"));
+app.use(express.static("public"));
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
+//Register view engine
 app.set("view engine", "ejs");
 app.set("views", "./views");
 
-var pendingTasks = [];
+//Connect to db
+mongoose
+  .connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((res) => {
+    console.log("Connected to db");
+    app.listen(port);
+  })
+  .catch((err) => {
+    console.log("Couldn't connect to MongoDB", err);
+  });
 
-app.get('/', async (req, res) => {
-  mongoose.connect(process.env.MONGO_URL)
-    .then(async () => {
-      const database = client.db("todo");
-      const tasks = await database.collection('pending').find().toArray();
-      pendingTasks = [...tasks];
-      
-      res.render('index.ejs', { tasks: pendingTasks });
+//Pending tasks
+app.get("/", (req, res) => {
+  res.redirect("/pending");
+});
+
+app.get("/pending", (req, res) => {
+  Pending.find()
+    .sort({ createdAt: -1 })
+    .then((result) => {
+      res.render("pending", { tasks: result });
+    });
+});
+
+//Add new task
+app.post("/pending", (req, res) => {
+  const pending = new Pending(req.body);
+
+  pending
+    .save()
+    .then((result) => {
+      res.redirect('/pending')
     })
     .catch((err) => {
-      console.log("Couldn't connect to MongoDB", err);
-    })
-})
+      console.log(err);
+    });
+});
 
-app.post('/completed', (req, res) => {
-  console.log('request', req.body);
-  // mongoose.connect(process.env.MONGO_URL)
-  // .then(async () => {
-  //   const database = client.db("todo");
-  //   const tasks = await database.collection('pending').find().toArray();
-  //   res.render('index.ejs', {tasks: tasks});
-  // })
-  // .catch(() => {
-  //   console.log("Couldn't connect to MongoDB");
-  // })
+app.get("/add-task", (req, res) => {
+  res.render("add-task");
+});
 
-})
+//Move to completed tasks
 
-app.get('/completed', (req, res) => {
-  mongoose.connect(process.env.MONGO_URL)
-    .then(async () => {
-      const database = client.db("todo");
-      const completed = await database.collection('completed').find().toArray();
-      res.render('completed.ejs', { tasks: completed });
-    })
-    .catch(() => {
-      console.log("Couldn't connect to MongoDB");
-    })
+app.delete("/pending/:id", (req, res) => {
+  const id = req.params.id;
+  const task = req.body.body;
+  console.log(id, task)
+});
 
-})
+//Completed tasks
+app.get("/completed", (req, res) => {
+  Complete.find()
+    .sort({ createdAt: -1 })
+    .then((result) => {
+      res.render("completed", { tasks: result });
+    });
+});
 
+//404
 app.use((req, res) => {
-  res.status(404).sendFile('./views/404.html', { root: __dirname })
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  res.status(404).sendFile("./views/404.html", { root: __dirname });
+});
