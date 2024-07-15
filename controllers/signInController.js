93b4b753609a5ed9a27352a8b969542a028f597a
Nguyễn Thịnh <110882542+nguyen-thinh109@users.noreply.github.com>
2024-07-15 10:01:59 +0700

    fix sign in

--- controllers/signInController.js
+++ controllers/signInController.js
@@ -1,23 +1,23 @@
 const usersDB = {
-  users: require('../models/users.json'),
+  users: require("../models/users.json"),
   setUser: function (users) {
-    this.users = users
-  }
+    this.users = users;
+  },
 };
-const fsPromises = require('fs').promises;
-const path = require('path');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const crypto = require('crypto');
-require('dotenv').config();
+const fsPromises = require("fs").promises;
+const path = require("path");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const crypto = require("crypto");
+require("dotenv").config();
 
 const showSignInPage = (req, res) => {
   res.render("sign-in");
 };
 
 const showSignUpPage = (req, res) => {
-  res.render('sign-up')
-}
+  res.render("sign-up");
+};
 
 const redirectToSignIn = (req, res) => {
   res.redirect("/sign-in");
@@ -29,88 +29,83 @@ const signIn = async (req, res) => {
   // console.log(username, password);
 
   if (!username || !password) {
-    return res.status(400).json({message: 'Username/ password is required!'})
+    return res.status(400).json({ message: "Username/ password is required!" });
   }
 
-  let foundUser = usersDB.users.find(user => user?.username === username);
+  let foundUser = usersDB.users.find((user) => user?.username === username);
 
   if (!foundUser) {
-    return res.status(404).json({message: 'User not found!'});
+    return res.status(404).json({ message: "User not found!" });
   }
 
   let isPwMatched = await bcrypt.compare(password, foundUser.password);
 
-  console.log('isPasswordMatched', isPwMatched)
+  console.log("isPasswordMatched", isPwMatched);
 
   if (isPwMatched) {
     // create JWTs
-    const accessToken = jwt.sign(
-      { "username": foundUser.username },
-      process.env.ACCESS_TOKEN_SECRET,
-      { expiresIn: '15m' }
-    );
+    const accessToken = jwt.sign({ username: foundUser.username }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "15m" });
 
-    const refreshToken = jwt.sign(
-      { "username": foundUser.username },
-      process.env.REFRESH_TOKEN_SECRET,
-      { expiresIn: '1d' }
-    );
+    const refreshToken = jwt.sign({ username: foundUser.username }, process.env.REFRESH_TOKEN_SECRET, { expiresIn: "1d" });
 
     // Saving refreshToken with current user
-    const otherUsers = usersDB.users.filter(person => person.username !== foundUser.username);
+    const otherUsers = usersDB.users.filter((person) => person.username !== foundUser.username);
     const currentUser = { ...foundUser, refreshToken };
     usersDB.setUser([...otherUsers, currentUser]);
-    await fsPromises.writeFile(
-        path.join(__dirname, '..', 'models', 'users.json'),
-        JSON.stringify(usersDB.users)
-    );
-    res.cookie('jwt', refreshToken, { httpOnly: true, sameSite: 'None', secure: true, maxAge: 24 * 60 * 60 * 1000 });
-    res.cookie('token', accessToken, { httpOnly: true, sameSite: 'None', secure: true, maxAge: 24 * 60 * 60 * 1000 })
-    return res.status(200).json({success: true});
+    
+    await fsPromises.writeFile(path.join(__dirname, "..", "models", "users.json"), JSON.stringify(usersDB.users));
+    res.cookie("jwt", refreshToken, { httpOnly: true, sameSite: "None", secure: true, maxAge: 24 * 60 * 60 * 1000 });
+    res.cookie("token", accessToken, { httpOnly: true, sameSite: "None", secure: true, maxAge: 24 * 60 * 60 * 1000 });
+    
+    return res.status(200).json({
+      success: true,
+      accessToken,
+      userData: {
+        username: foundUser.username,
+        phoneNumber: foundUser.phoneNumber,
+        email: foundUser.email,
+      },
+    });
   } else {
-    return res.status(404).json({message: 'User not found!'});
+    return res.status(404).json({ message: "User not found!" });
   }
 };
 
 const signUp = async (req, res) => {
   // console.log(req.body);
-  const {username , password, phoneNumber, email} = req.body
-  
+  const { username, password, phoneNumber, email } = req.body;
+
   //Check duplicate username
-  let isUsernameDulpicated = usersDB.users.find(user => user?.username === username)
+  let isUsernameDulpicated = usersDB.users.find((user) => user?.username === username);
   if (isUsernameDulpicated) {
-    return res.status(400).json({message: 'Username existed!'})
+    return res.status(400).json({ message: "Username existed!" });
   }
 
   //Check duplicate phoneNumber
-  let isPhoneNumberDulpicated = usersDB.users.find(user => user?.phoneNumber === phoneNumber)
+  let isPhoneNumberDulpicated = usersDB.users.find((user) => user?.phoneNumber === phoneNumber);
   if (isPhoneNumberDulpicated) {
-    return res.status(400).json({message: 'Phone number existed!'})
+    return res.status(400).json({ message: "Phone number existed!" });
   }
 
   //Check duplicate email
-  let isEmailDuplicated = usersDB.users.find(user => user?.email === email)
-  
+  let isEmailDuplicated = usersDB.users.find((user) => user?.email === email);
+
   if (isEmailDuplicated) {
-    return res.status(400).json({message: 'Email existed!'})
+    return res.status(400).json({ message: "Email existed!" });
   }
 
   try {
     //encrypt the password
     const hashedPwd = await bcrypt.hash(password, 10);
-    const newUser = {username, phoneNumber, email, password: hashedPwd, id: crypto.randomUUID()}
+    const newUser = { username, phoneNumber, email, password: hashedPwd, id: crypto.randomUUID() };
     //set new user
     usersDB.setUser([...usersDB.users, newUser]);
-  
-    await fsPromises.writeFile(
-      path.join(__dirname, '..', 'models', 'users.json'),
-      JSON.stringify(usersDB.users)
-    );
-    
-    res.status(201).json({success: true});
-    
+
+    await fsPromises.writeFile(path.join(__dirname, "..", "models", "users.json"), JSON.stringify(usersDB.users));
+
+    res.status(201).json({ success: true });
   } catch (error) {
-    res.status(500).json({ 'message': err.message });
+    res.status(500).json({ message: err.message });
   }
 };
 
@@ -119,7 +114,7 @@ module.exports = {
   showSignInPage,
   signIn,
   signUp,
-  showSignUpPage
+  showSignUpPage,
 };
 
 // HttpStatusCode {
